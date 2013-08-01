685eab075a3b8dda01ee27f67c51de3d0abd3cb2
Dave Paola <dpaola2@gmail.com>
2013-08-01 13:26:46 -0700

    updated docstring

--- ChordedKeyboard.ino
+++ ChordedKeyboard.ino
@@ -1,32 +1,16 @@
 /*
-  Button
- 
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
- 
- 
- The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Button
- */
 
-// constants won't change. They're used here to 
+Chorded Keyboard (a la Doug Engelbart's 1968 demo)
+Dave Paola, started Aug 1, 2013
+
+Connect the five buttons to pins 2 - 6, as outlined here:
+http://www.arduino.cc/en/Tutorial/Pushbutton
+
+
+*/
+
 // set pin numbers:
-const int firstButtonPin = 2;     // the number of the pushbutton pin
+const int firstButtonPin = 2;
 const int secondButtonPin = 3;
 const int thirdButtonPin = 4;
 const int fourthButtonPin = 5;
